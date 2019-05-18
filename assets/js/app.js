f8e1cf9692a60834a95d2659d397f07f4cde2539
yehorbk <e.bublyk1010@gmail.com>
2019-05-18 14:42:10 +0300

    Bug with speed fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,19 +33,20 @@ delorean.src = delorean_car_image;
 
 // Game Variables //
 
+var block_size = 2;
+var ground = 304
+
 var time = "80s";
 
 var player_posX = 200;
-var player_posY = 304;
+var player_posY = ground;
 var car_speed = 0;
 
 var background_posX = 0;
-var world_speed = 1;
+var world_speed = 0;
 
 var speed_count = document.getElementById("speed_count");
 var engine_ready = document.getElementById("engine_ready");
-
-var block_size = 2;
 
 var isCar = true;
 var isReadyTeleport = false;
@@ -98,7 +99,7 @@ function draw() {
 	context.drawImage(background, background_posX + background.width, 0);
 	context.drawImage(delorean, player_posX, player_posY);
 	speed_count.textContent = "Speed: " + car_speed;
-	
+
 	if (background_posX < background.width * -1) {
 		background_posX = 0;
 	}
@@ -134,7 +135,7 @@ function changeTime() {
 	}
 	
 	isReadyTeleport = false;
-	car_speed = 0;
+	car_speed = 2;
 	world_speed = 1;
 	player_posX = 200;
 
@@ -151,9 +152,9 @@ function delorean_move(direction) {
 			}
 		}
 	} else {
-		if (player_posY + block_size <= window_height - delorean.height) {
+		if (player_posY + block_size <= ground) {
 			player_posY += block_size;
-			if (!isCar && player_posY == 304) {
+			if (!isCar && player_posY == ground) {
 				delorean.src = delorean_car_image;
 				isCar = true;
 			}
@@ -163,13 +164,13 @@ function delorean_move(direction) {
 
 function changeSpeed(increase) {
 	if (increase) {
-		if (player_posX + block_size <= window_width - delorean.width && car_speed <= 143) {
+		if (player_posX + block_size <= window_width - delorean.width && car_speed <= 109) {
 			player_posX += block_size;
 			car_speed += 1;
 			world_speed += 0.5;
 		}
 	} else {
-		if (player_posX - block_size >= 0 && car_speed >= 0) {
+		if (player_posX - block_size >= 0 && car_speed > 0) {
 			player_posX -= block_size;
 			car_speed -= 1;
 			world_speed -= 0.5;
